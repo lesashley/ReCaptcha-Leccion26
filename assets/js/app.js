1f217c86f5c44f3afe4b177db74f2c688a25218d
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-28 13:44:14 -0500

    creando cambio de color para el captcha

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,15 @@ var codigoIngresado = document.getElementsByClassName("valor")[0];
 
 function random() {
   var letras = "ABCDEFJKLMNOPQRSTUVWXYZ1234567890!'#$";
+  var hexadecimal = "0123456789ABCDEF".split("");
+  var c = "#";
   var recaptcha = 0;
-  for(var i = 0; i < 8; i++){
+  for(var i = 0; i < 7; i++){
     var rango = Math.floor(Math.random()*letras.length);
+    c += hexadecimal[Math.floor(Math.random() * 16)];
     recaptcha += letras.substr(rango,1);
   }
+  codigo.style.color = c;
   codigo.value = recaptcha;
 }
 random();
